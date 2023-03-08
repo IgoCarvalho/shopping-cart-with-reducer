752b42510d36ee3903faeb38ec026ffe0abbba13
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-08 19:36:05 -0300

    ✨ add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { GlobalStyles } from './styles/global';
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </>
   );
 }
