8bfb2fabe6eb9c4770f422bf206ea7916eec88f7
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-08 22:35:10 -0300

    ✨ listing shopping items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import { Header } from './components/Header/Header';
 import { ShoppingCard } from './components/ShoppingCard/ShoppingCard';
 import { ShoppingContent } from './components/ShoppingContent/ShoppingContent';
+import { ShoppingData } from './types/shopping';
 
 import { GlobalStyles } from './styles/global';
+
+const shoppingData: ShoppingData[] = [
+  {
+    id: '1',
+    title: 'Nike Air',
+    price: {
+      value: 289,
+      discount: 180,
+    },
+    image: {
+      background: '#f7c29b',
+      src: '/images/shoes1.png',
+    },
+    colors: ['#D7263D', '#3083dcff', '#7dde92ff', '#2ebfa5ff'],
+  },
+  {
+    id: '2',
+    title: 'Nike Flex',
+    price: {
+      value: 149,
+    },
+    image: {
+      background: '#7be498',
+      src: '/images/shoes2.png',
+    },
+    colors: ['#33658aff', '#86bbd8ff', '#f6ae2dff', '#f26419ff'],
+  },
+];
 
 export function App() {
   return (
@@ -12,7 +41,9 @@ export function App() {
       <Header />
 
       <ShoppingContent>
-        <ShoppingCard />
+        {shoppingData.map((item) => (
+          <ShoppingCard key={item.id} data={item} />
+        ))}
       </ShoppingContent>
     </>
   );
--- src/components/ShoppingContent/ShoppingContent.styles.ts
+++ src/components/ShoppingContent/ShoppingContent.styles.ts
@@ -5,4 +5,6 @@ export const Container = styled.main`
   max-width: 1120px;
   margin: 0 auto;
   padding: 24px 0;
+  display: flex;
+  gap: 32px;
 `;
